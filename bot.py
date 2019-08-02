237e5ec4d12da897814214ba37ed2631d2e9977f
eatiinngg <s88194@gmail.com>
2019-08-02 18:21:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='[')
 
 @bot.event
-async def on_reeady();
+async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('NjA2Nzc5MDc0NTM4NTA0MTk0.XUQBMw.C2hgfWkLgtsGE0JlW0ljnQ7yV4U')
+bot.run('NjA2Nzc5MDc0NTM4NTA0MTk0.XUQLdQ.eKk1lmtftCOY2Nh0X0Nu8kQCHPg')

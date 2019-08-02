fffeba5255675693c03208437f03802113285d0b
eatiinngg <s88194@gmail.com>
2019-08-02 23:32:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,15 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('NjA2Nzc5MDc0NTM4NTA0MTk0.XUQLdQ.eKk1lmtftCOY2Nh0X0Nu8kQCHPg')
+@bot.event 
+async def on_member_join(member):
+    channel = bot.get_channel(606867059837501470)
+    await channel.send(f'{member}上線囉~~')
+
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(606867165123182595)
+    await channel.send(f'{member}離線囉~~')
+
+bot.run('NjA2Nzc5MDc0NTM4NTA0MTk0.XURW_w.lqz6GkZyoIj1G-gfSqWs6Pr3-QQ')

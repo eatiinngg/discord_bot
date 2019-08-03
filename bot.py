c5726bcaa83b817c59f76e2fb9947ed01ffd52a5
eatiinngg <s88194@gmail.com>
2019-08-03 11:50:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
+import json
 
-bot = commands.Bot(command_prefix='[')
+with open('setting.json', mode='r', encoding='utf8') as jfile:
+    jdata=json.load(jfile)
+
+bot = commands.Bot(command_prefix='-')
 
 @bot.event
 async def on_ready():
@@ -9,17 +13,16 @@ async def on_ready():
 
 @bot.event 
 async def on_member_join(member):
-    channel = bot.get_channel(606867059837501470)
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
     await channel.send(f'{member}上線囉~~')
-
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(606867165123182595)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(f'{member}離線囉~~')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-bot.run('NjA2Nzc5MDc0NTM4NTA0MTk0.XURZmQ.nmM585zPMp-TOeQvQ68NtIOlO48')
+bot.run(jdata['TOKEN'])
